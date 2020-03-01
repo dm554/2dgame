25d911700908169c52e96d115c5f1acb6c10201d
dm554 <dm554@njit.edu>
2020-03-01 16:15:04 -0500

    walking animations

--- src/2dgame/Player.c
+++ src/2dgame/Player.c
@@ -4,6 +4,8 @@
 
 #define ES_DEAD 1
 
+static int walking = 0;
+
 void player_move(Entity *self){
 	//slog("Player Move Function called");
 	Uint8 *buttons = SDL_GetKeyboardState(NULL);
@@ -13,41 +15,58 @@ void player_move(Entity *self){
 	if (buttons[SDL_SCANCODE_RIGHT] && buttons[SDL_SCANCODE_UP]){
 		self->position.x += 1.5;
 		self->position.y -= 0.5;
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_RIGHT] && buttons[SDL_SCANCODE_DOWN]){
 		self->position.x += 1.5;
 		self->position.y += 0.5;
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_LEFT] && buttons[SDL_SCANCODE_UP]){
 		self->position.x -= 1.5;
 		self->position.y -= 0.5;
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_LEFT] && buttons[SDL_SCANCODE_DOWN]){
 		self->position.x -= 1.5;
 		self->position.y += 0.5;
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_RIGHT]){
 		self->position.x += 3;
-		//self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_LEFT]){	
 		self->position.x -= 3;
-		//self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_UP]){
 		self->position.y -= 2;
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
 	if (buttons[SDL_SCANCODE_DOWN]){
 		self->position.y += 2;
+		int walking = 1;
+		self->sprite = gf2d_sprite_load_all("images/playerWalk.png", 110, 200, 5);
 		return;
 	}
+
+	int walking = 0;
+	self->sprite = gf2d_sprite_load_all("images/playerSprite2.png", 160, 200, 5);
 }
 
 void player_think(Entity *self){
